7b710a82198f93a34b03e7905c67d99bb738e800
sutetotanuki <sutetotanuki@gmail.com>
2013-04-14 14:12:10 +0900

    add nil case

--- spec/mameconf_spec.rb
+++ spec/mameconf_spec.rb
@@ -28,11 +28,27 @@ describe Mameconf do
         
         mameconf :host, default: "localhost"
         mameconf :port, default: 3337
-      end.new
+      end
     end
 
     it "returns hash that included default values" do
-      subject.to_hash.should eq ({ host: "localhost", port: 3337 })
+      subject.new.to_hash.should eq ({ host: "localhost", port: 3337 })
+    end
+
+    context "has no default value attribute" do
+      before do
+        subject.class_eval do
+          mameconf :namespace
+        end
+      end
+
+      it "has no dafalut value attribtue" do
+        subject.new.namespace.should be_nil
+      end
+
+      it "has attributes that has default value" do
+        subject.new.host.should eq "localhost"
+      end
     end
   end
 end
