024925054e7c69e21c21fe406133af31a8ddc8e6
sutetotanuki <sutetotanuki@gmail.com>
2013-04-14 14:06:54 +0900

    Add 'to_hash' method

--- lib/mameconf.rb
+++ lib/mameconf.rb
@@ -6,7 +6,15 @@ module Mameconf
   end
 
   module ClassMethods
+    attr_accessor :mameconf_attr_names
+
+    def mameconf_attr_names
+      @mameconf_attr_names ||= []
+    end
+    
     def mameconf(name, options={})
+      mameconf_attr_names << name
+      
       default_value = options[:default]
       class_eval <<-RUBY, __FILE__, __LINE__ + 1
         def #{name}
@@ -15,6 +23,15 @@ module Mameconf
 
         def #{name}=(val)
           @#{name} = val
+        end
+
+        def to_hash
+          ret = {}
+          self.class.mameconf_attr_names.each do |attr|
+            attr_sym = attr.to_sym
+            ret[attr_sym] = self.__send__(attr_sym)
+          end
+          ret
         end
       RUBY
     end
--- spec/mameconf_spec.rb
+++ spec/mameconf_spec.rb
@@ -1,27 +1,38 @@
 require "spec_helper"
 
 describe Mameconf do
-  class Included
-    include Mameconf
-  end
-
   describe "defualt value" do
     subject do
-      Included.class_eval do
+      Class.new do
+        include Mameconf
         mameconf :host, default: "localhost"
       end
-      Included
     end
 
     it "returns default valeu if not present" do
       subject.new.host.should eq "localhost"
     end
 
-    it "allows override default value" do
+    it "allows to override default value" do
       instance = subject.new
       instance.host = "google.com"
 
       instance.host.should eq "google.com"
+    end
+  end
+
+  describe "#to_hash" do
+    subject do
+      Class.new do
+        include Mameconf
+        
+        mameconf :host, default: "localhost"
+        mameconf :port, default: 3337
+      end.new
+    end
+
+    it "returns hash that included default values" do
+      subject.to_hash.should eq ({ host: "localhost", port: 3337 })
     end
   end
 end
